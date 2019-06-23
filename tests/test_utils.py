1513f856d221fd5a76ada815dfb4759ff6c0b4dd
yasufumi <yasufumi.taniguchi@gmail.com>
2019-06-23 22:02:19 +0900

    Update test cases for easyfile.TextFile

---
+++ tests/test_utils.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+import tempfile
+import os
+
+from easyfile import utils
+
+
+class UtilsTestCase(TestCase):
+
+    def setUp(self):
+        self.fp = tempfile.NamedTemporaryFile()
+
+    def tearDown(self):
+        self.fp.close()
+
+    def test_open(self):
+        with utils.open(self.fp.name, os.O_RDWR) as fd:
+            self.assertIsInstance(fd, int)
