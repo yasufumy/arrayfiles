c51539dfa67b3cca8127149471c3e98054ac2e52
yasufumi <yasufumi.taniguchi@gmail.com>
2019-12-20 23:02:23 +0900

    Add support for Python 3.8

--- setup.py
+++ setup.py
@@ -22,5 +22,6 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
