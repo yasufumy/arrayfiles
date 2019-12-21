2160ec0d2efd6f79e1b6a6e075c6c117f1f71394
yasufumi <yasufumi.taniguchi@gmail.com>
2019-12-21 22:09:16 +0900

    Add new api to load text/csv

--- easyfile/__init__.py
+++ easyfile/__init__.py
@@ -1,3 +1,6 @@
 from easyfile.core import TextFile  # NOQA
 from easyfile.core import CsvFile  # NOQA
 from easyfile.core import CustomNewlineTextFile  # NOQA
+
+from easyfile.core import read_text  # NOQA
+from easyfile.core import read_csv  # NOQA
